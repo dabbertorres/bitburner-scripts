7ecd51ce8c503b791da82320d11fb7d60084c5d2
Alec Iverson <adiverso93@gmail.com>
2022-04-17 10:43:05 -0700

    stats.js: add hacking information

--- stats.js
+++ stats.js
@@ -7,9 +7,15 @@ export async function main(ns) {
 	ns.tprintf("\tSec:   %f / %f\n", ns.getServerSecurityLevel(target), ns.getServerMinSecurityLevel(target));
 	ns.tprintf("\tRAM:   %f / %f\n", ns.getServerUsedRam(target), ns.getServerMaxRam(target));
 	ns.tprintf("\tGrow:  %f\n", ns.getServerGrowth(target));
-	ns.tprintf("\tTo Hack:\n");
+
+	ns.tprintf("\tTo Root:\n");
 	ns.tprintf("\t\tPorts: %f\n", ns.getServerNumPortsRequired(target));
 	ns.tprintf("\t\tLevel: %f\n", ns.getServerRequiredHackingLevel(target));
+
+	ns.tprintf("\tTo Hack:\n");
+	ns.tprintf("\t\tHack Time: %s\n", ns.tFormat(ns.getHackTime(target)));
+	ns.tprintf("\t\tGrow Time: %s\n", ns.tFormat(ns.getGrowTime(target)));
+	ns.tprintf("\t\tWeak Time: %s\n", ns.tFormat(ns.getWeakenTime(target)));
 }
 
 export function autocomplete(data, args) {
