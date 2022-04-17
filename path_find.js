885949b4f382498c1f5eab26e176f2eacf980741
Alec Iverson <adiverso93@gmail.com>
2022-04-17 10:41:30 -0700

    rename find.js => path_find.js

---
+++ path_find.js
@@ -0,0 +1,44 @@
+export function autocomplete(data, args) {
+	return [...data.servers];
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+    if (ns.args.length !== 1) {
+        ns.tprint("must provide a single target server name.");
+        return;
+    }
+
+    const target = ns.args[0];
+    const source = ns.getHostname();
+    const chain = find(ns,target,source,[],[]);
+
+    if (!chain) {
+        ns.tprintf("Couldn't find %s.", target);
+        return;
+    }
+
+    ns.tprint(chain.join(" -> "));
+}
+
+/** 
+ * @param {NS} ns
+ * @param {string} target
+ * @param {string} source
+ * @param {string[]} chain
+ * @param {string[]} ignore
+ * 
+ * @return {string[]}
+ */
+function find(ns, target, source, chain, ignore) {
+    for (let server of ns.scan(source)) {
+        if (ignore.includes(server)) continue;
+
+        if (server === target) return [...chain, server];
+
+        const path = find(ns, target, server, [...chain, server], [...ignore, source]);
+        if (path) return path;
+    }
+
+    return null;
+}
